421c698aca7ebeed03e83c48349b228ffc2ecc40
fojletuhin <fojletuhin@gmail.com>
2024-09-22 13:04:10 +0600

    complete Navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -4,6 +4,7 @@ import ball from "../../../public/cricket.ball.fill.svg"
 import icon from "../../../public/Icon.svg"
 import abstractLeft from "../../../public/Abstract Design left.svg"
 import abstractRight from "../../../public/Abstract Design right.svg"
+import logo from "../../../public/logo.svg"
 import Image from 'next/image';
 import Link from 'next/link';
 import { MenuIcon } from 'lucide-react';
@@ -11,12 +12,13 @@ import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
 import { Button } from '../ui/button';
 
 const Navbar = () => {
+    
     return (
         <div>
 
             {/* 1st Div */}
             <div className='mobile:h-[94px] mobile:px-4 mobile:pt-10 pb-[14px] medium:h-[68px] medium:py-[14px] medium:px-[80px] bg-[#1A3129] text-[#FFFFFF] border-b-[1px] border-0 border-b-[#234338] relative overflow-hidden'>
-                <div className='h-[40px] rounded-[6px] border-[1px] py-[10px] mobile:px-1  bg-[#234338] leading-tight border-[#2C5446] relative z-30'>
+                <div className=' rounded-[6px] border-[1px] py-[10px] mobile:px-1  bg-[#234338] leading-tight border-[#2C5446] relative z-30'>
                     <div className='flex justify-center '>
 
                         <Image
@@ -65,50 +67,66 @@ const Navbar = () => {
 
 
             {/* 2nd Div  */}
-            <div className="flex items-center justify-between px-4 py-2 bg-white dark:bg-gray-800">
+            <div className="flex items-center justify-between py-5 px-4 medium:px-20 medium:py-4 bg-[#1A3129] text-[#FFFFFF] border-b-[1px] border-0 border-b-[#EEF8D3] medium:h-[77px] mobile:h-[74px]">
                 <Link href="#" className="flex items-center gap-2" prefetch={false}>
-                    <span className="text-lg font-semibold">Acme Inc</span>
+                    <Image
+                        src={logo}
+                        alt="Picture of the ball"
+                    />
+                    <span className="text-lg font-semibold">Nutritionist</span>
                 </Link>
-                <div className="hidden md:flex gap-4">
-                    <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
+                <div className="hidden medium:flex gap-5 items-center">
+                    <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
                         Home
                     </Link>
-                    <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
+                    <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
                         About
                     </Link>
-                    <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                        Services
+                    <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
+                        Team
                     </Link>
-                    <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                        Portfolio
+                    <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
+                        Process
                     </Link>
-                    <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                        Contact
+                    <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
+                        Pricing
+                    </Link>
+                    <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
+                        Blog
+                    </Link>
+                    <Link href="#" className="text-[14px] ml-1 font-semibold rounded-[6px] bg-[#CBEA7B] px-4 py-3 text-[#262626] hover:underline underline-offset-4" prefetch={false}>
+                        Contact Us
                     </Link>
                 </div>
                 <Sheet>
                     <SheetTrigger asChild>
-                        <Button variant="outline" size="icon" className="lg:hidden">
-                            <MenuIcon className="h-6 w-6" />
+                        <Button variant="outline" size="icon" className="lg:hidden group bg-[#1A3129] border-0 hover:bg-[#EEF8D3]">
+                            <MenuIcon className="h-6 w-6 text-[#EEF8D3] p-0 group-hover:text-[#1A3129]" />
                             <span className="sr-only">Toggle navigation menu</span>
                         </Button>
                     </SheetTrigger>
                     <SheetContent side="left">
                         <div className="grid w-[200px] p-4">
-                            <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
+                            <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
                                 Home
                             </Link>
-                            <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
+                            <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
                                 About
                             </Link>
-                            <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                                Services
+                            <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
+                                Team
                             </Link>
-                            <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                                Portfolio
+                            <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
+                                Process
                             </Link>
-                            <Link href="#" className="text-lg font-medium hover:underline underline-offset-4" prefetch={false}>
-                                Contact
+                            <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
+                                Pricing
+                            </Link>
+                            <Link href="#" className="text-[14px] font-semibold rounded-[6px] hover:underline underline-offset-4" prefetch={false}>
+                                Blog
+                            </Link>
+                            <Link href="#" className="text-[14px] font-semibold rounded-[6px] bg-[#CBEA7B] px-4 py-3 text-[#262626] hover:underline underline-offset-4" prefetch={false}>
+                                Contact Us
                             </Link>
                         </div>
                     </SheetContent>
