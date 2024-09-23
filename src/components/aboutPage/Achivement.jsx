adff7dd735dfde89eb0fdd4631cbbb8c974ee7e4
fojletuhin <fojletuhin@gmail.com>
2024-09-24 03:28:48 +0600

    complete about page

--- src/components/aboutPage/Achivement.jsx
+++ src/components/aboutPage/Achivement.jsx
@@ -89,14 +89,14 @@ const Achivement = () => {
             </div>
 
 
-            <div className='mt-[50px] h-[218px] laptop:mt-[100px] p-[60px]  bg-[#F6FBE9] border-[1px] border-[#E5F5BD] rounded-[10px] relative overflow-hidden'>
-                <div className='flex gap-[150px]'>
+            <div className='mt-[50px]  laptop:mt-[100px] medium:p-[60px] p-10  bg-[#F6FBE9] border-[1px] border-[#E5F5BD] rounded-[10px] relative overflow-hidden'>
+                <div className='tablet:flex gap-[150px]'>
                     <div>
-                        <p className='font-semibold text-[#262626] text-[26px] mb-[10px]'>We Are Proud of Our Achievements</p>
-                        <p className='font-medium text-[#333333]'>But our ultimate satisfaction comes from seeing our clients achieve their goals and live healthier, happier lives. Join Nutritionist <br /> today and embark on your own transformative journey towards optimal health and well-being.</p>
+                        <p className='font-semibold text-[#262626] medium:text-[26px] text-[22px] mb-[10px]'>We Are Proud of Our Achievements</p>
+                        <p className='font-medium text-[#333333]'>But our ultimate satisfaction comes from seeing our clients achieve their goals and live healthier, happier lives. Join Nutritionist  today and embark on your own transformative journey towards optimal health and well-being.</p>
                     </div>
                     <div className='flex items-center'>
-                        <p className='bg-[#CBEA7B] py-[14px] px-5 rounded-[6px]'>Book A Demo</p>
+                        <p className='bg-[#CBEA7B] tablet:w-[128px] mt-5 tablet:mt-0 w-full text-center font-semibold text-[14px]  py-[14px] px-5 rounded-[6px]'>Book A Demo</p>
                     </div>
                 </div>
                 <Image
