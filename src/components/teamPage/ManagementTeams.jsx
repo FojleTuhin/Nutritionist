93026922f42e56f8bc28056a31fb12e006002f10
fojletuhin <fojletuhin@gmail.com>
2024-09-24 20:54:55 +0600

    add team component

--- src/components/teamPage/ManagementTeams.jsx
+++ src/components/teamPage/ManagementTeams.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Tabs, TabsContent, TabsList, TabsTriggerManagement } from "@/components/ui/tabs"
+import TeamOne from './TeamOne';
+import TeamTwo from './TeamTwo';
+import TeamThree from './TeamThree';
+import TeamFour from './TeamFour';
+import TeamFive from './TeamFive';
 const ManagementTeams = () => {
     return (
         <div>
-            <div className='bg-[#234338] p-5 medium:py-5 medium:px-20 rounded-b-[10px] text-[#FFFFFF] font-semibold flex gap-5 medium:justify-between items-center overflow-auto overflow-x-scroll whitespace-nowrap scrollbar-hide'>
-                <p className='hover:py-3 hover:px-[18px] hover:bg-[#2C5446] hover:border-[1px] hover:border-[#356554] hover:rounded-[6px]'>Management Team</p>
-                <p className='hover:py-3 hover:px-[18px] hover:bg-[#2C5446] hover:border-[1px] hover:border-[#356554] hover:rounded-[6px]'>Nutritionists and Dietitians</p>
-                <p className='hover:py-3 hover:px-[18px] hover:bg-[#2C5446] hover:border-[1px] hover:border-[#356554] hover:rounded-[6px]'>Customer Support</p>
-                <p className='hover:py-3 hover:px-[18px] hover:bg-[#2C5446] hover:border-[1px] hover:border-[#356554] hover:rounded-[6px]'>Marketing and Communications</p>
-                <p className='hover:py-3 hover:px-[18px] hover:bg-[#2C5446] hover:border-[1px] hover:border-[#356554] hover:rounded-[6px]'>Technology and Development</p>
-            </div>
+           
 
 
-            <div>
+            <div className=" ">
 
-                <Tabs defaultValue="Management Team" className='flex flex-col items-center mt-10'>
-                    <TabsList className=" w-full h-[96px] bg-[#234338] p-5 medium:py-5 medium:px-20 rounded-b-[10px] rounded-t-none flex gap-5 justify-between  overflow-auto overflow-x-scroll whitespace-nowrap scrollbar-hide ">
+                <Tabs defaultValue="Management Team" className='flex flex-col items-center'>
+                    <TabsList className=" w-full h-[96px] bg-[#234338] p-5 medium:py-5 medium:px-20 rounded-b-[10px] rounded-t-none flex gap-5 justify-between  overflow-auto overflow-x-scroll whitespace-nowrap scrollbar-hide mb-[50px]">
                         <TabsTriggerManagement value="Management Team" className="py-3 px-[18px] whitespace-nowrap text-[16px] text-[#FFFFFF] font-semibold">Management Team</TabsTriggerManagement>
                         <TabsTriggerManagement value="Nutritionists and Dietitians" className="py-3 px-[18px] whitespace-nowrap text-[16px] text-[#FFFFFF] font-semibold">Nutritionists and Dietitians</TabsTriggerManagement>
                         <TabsTriggerManagement value="Customer Support" className="py-3 px-[18px] whitespace-nowrap text-[16px] text-[#FFFFFF] font-semibold">Customer Support</TabsTriggerManagement>
@@ -24,11 +23,11 @@ const ManagementTeams = () => {
                     </TabsList>
 
 
-                    <TabsContent value="Management Team">management Team</TabsContent>
-                    <TabsContent value="Nutritionists and Dietitians">Nutritionists and Dietitians</TabsContent>
-                    <TabsContent value="Customer Support">Customer Support</TabsContent>
-                    <TabsContent value="Marketing and Communications">Marketing and Communications</TabsContent>
-                    <TabsContent value="Technology and Development">Technology and Development</TabsContent>
+                    <TabsContent value="Management Team"><TeamOne/></TabsContent>
+                    <TabsContent value="Nutritionists and Dietitians"><TeamTwo/></TabsContent>
+                    <TabsContent value="Customer Support"><TeamThree/></TabsContent>
+                    <TabsContent value="Marketing and Communications"><TeamFour/></TabsContent>
+                    <TabsContent value="Technology and Development"><TeamFive/></TabsContent>
                 </Tabs>
 
 
---
+++ src/components/teamPage/TeamTwo.jsx
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+import React from 'react';
+import img1 from '../../../public/22.png'
+import img2 from '../../../public/23.png'
+import img3 from '../../../public/24.png'
+import img4 from '../../../public/25.png'
+
+const TeamTwo = () => {
+    return (
+        <div className='flex gap-5 flex-wrap justify-center'>
+            <div className='w-[305] relative h-[384px]'>
+                <Image
+                    src={img1}
+                    alt='image' />
+
+                <div className='p-5 bg-[#F6FBE9] rounded-tr-[20px] w-[245px] absolute bottom-0'>
+                    <p className='font-semibold text-[20px] text-[#262626]'>Sarah Mitchell</p>
+                    <p className='font-medium text-[#4C4C4D]'>Founder and CEO</p>
+                </div>
+            </div>
+            <div className='w-[305] relative h-[384px]'>
+                <Image
+                    src={img2}
+                    alt='image' />
+
+                <div className='p-5 bg-[#F6FBE9] rounded-tr-[20px] w-[245px] absolute bottom-0'>
+                    <p className='font-semibold text-[20px] text-[#262626]'>Emily Thompson</p>
+                    <p className='font-medium text-[#4C4C4D]'>Chief Operating Officer</p>
+                </div>
+            </div>
+            <div className='w-[305] relative h-[384px]'>
+                <Image
+                    src={img3}
+                    alt='image' />
+
+                <div className='p-5 bg-[#F6FBE9] rounded-tr-[20px] w-[245px] absolute bottom-0'>
+                    <p className='font-semibold text-[20px] text-[#262626]'>John Davis</p>
+                    <p className='font-medium text-[#4C4C4D]'>Chief Financial Officer</p>
+                </div>
+            </div>
+            <div className='w-[305] relative h-[384px]'>
+                <Image
+                    src={img4}
+                    alt='image' />
+
+                <div className='p-5 bg-[#F6FBE9] rounded-tr-[20px] w-[245px] absolute bottom-0'>
+                    <p className='font-semibold text-[20px] text-[#262626]'>Rachel Adams</p>
+                    <p className='font-medium text-[#4C4C4D]'>Chief Marketing Officer</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TeamTwo;
