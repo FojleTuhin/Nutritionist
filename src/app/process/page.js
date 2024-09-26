a52b4192e283cc6f6c4013a273f5f1f2abe2357d
fojletuhin <fojletuhin@gmail.com>
2024-09-26 12:21:58 +0600

    add process page banner

--- src/app/process/page.js
+++ src/app/process/page.js
@@ -1,9 +1,14 @@
+import HowItWorks from '@/components/processPage/HowItWorks';
+import TeamExpert from '@/components/teamPage/TeamExpert';
 import React from 'react';
 
 const page = () => {
     return (
-        <div>
-            
+        <div className='px-4 tablet:px-10 medium:px-[60px]  laptop:px-20 pt-20 '>
+            <TeamExpert heading='Your Journey to Health and Wellness' subHeading='At Nutritionist, we believe in providing a personalized and comprehensive approach to help you achieve your health and wellness goals. Our "How it Works" process is designed to guide you through each step of your journey, ensuring that you receive the support, knowledge, and tools you need to succeed. Here is a detailed breakdown of our process' />
+
+
+            <HowItWorks/>
         </div>
     );
 };
---
+++ src/components/processPage/HowItWorks.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HowItWorks = () => {
+    return (
+        <div className='mt-[50px] medium:mt-[80px] laptop:mt-[100px]'>
+            <div className='text-center medium:w-[55%] mx-auto'>
+                <p className='text-[#262626] text-[28px] font-bold'>How It Works</p>
+                <p className='text-[#333333] text-[14px] font-medium mt-1'>We provide a step-by-step guide on how to get started on your journey towards better health and nutrition. We are here to simplify the process and make it easy for you to navigate our platform and access the resources you need to achieve your goals. Here's how it works</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default HowItWorks;
