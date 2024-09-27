a0ab03753db623c87c1a22bbc52a641746a6c5ac
fojletuhin <fojletuhin@gmail.com>
2024-09-27 23:22:24 +0600

    add loading element

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,7 +1,11 @@
+import { CircleLoader } from "react-spinners";
+
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     return (
-        <p>loadiong--------</p>
-    
+
+        <div className="flex justify-center items-center min-h-screen">
+            <CircleLoader />
+        </div>
     )
-  }
+}
