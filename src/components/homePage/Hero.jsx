4414db1666bd8f0a4a322b4db2e5c6de8742e429
fojletuhin <fojletuhin@gmail.com>
2024-10-04 01:43:45 +0600

    animated button add

--- src/components/homePage/Hero.jsx
+++ src/components/homePage/Hero.jsx
@@ -29,8 +29,27 @@ const Hero = () => {
                             <p className='text-[#333333] font-medium'>Welcome to Nutritionist, your partner in achieving optimal health through personalized nutrition coaching. Our certified nutritionists are here to guide you on your weight loss journey, providing customized plans and ongoing support. Start your transformation today and experience the power of personalized nutrition coaching.</p>
                         </div>
                         <div className='flex gap-[14px]'>
-                            <p className='py-[14px] px-4 bg-[#CBEA7B] rounded-[6px] text-[#262626] text-[14px] font-semibold'>Get Starter Today</p>
-                            <p className='py-[14px] px-5 bg-[#F6FBE9] rounded-[6px] text-[#262626] text-[14px] font-semibold border-[#E5F5BD] border-[1px]'>Book a Demo</p>
+                            {/* <p className='py-[14px] px-4 bg-[#CBEA7B] rounded-[6px] text-[#262626] text-[14px] font-semibold'>Get Starter Today</p> */}
+
+                            <div class="relative inline-flex  group">
+                                <div
+                                    class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                                </div>
+                                <a href="#" title="Get quote now"
+                                    class="relative inline-flex items-center justify-center py-[14px] px-5 text-[14px] font-semibold text-[#262626] transition-all duration-200 bg-[#CBEA7B]  rounded-[6px] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:[#CBEA7B]"
+                                    role="button">Get Starter Today
+                                </a>
+                            </div>
+                            {/* <p className='py-[14px] px-5 bg-[#F6FBE9] rounded-[6px] text-[#262626] text-[14px] font-semibold border-[#E5F5BD] border-[1px]'>Book a Demo</p> */}
+                            <div class="relative inline-flex  group">
+                                <div
+                                    class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                                </div>
+                                <a href="#" title="Get quote now"
+                                    class="relative border-[#E5F5BD] border-[1px] inline-flex items-center justify-center py-[14px] px-5 text-[14px] font-semibold text-[#262626] transition-all duration-200 bg-[#F6FBE9]   rounded-[6px] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:[#F6FBE9]"
+                                    role="button">Book a Demo
+                                </a>
+                            </div>
 
                         </div>
 
